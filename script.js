534090bc409390f1856a7d71f831d10a46b3c473
nelio1977 <69060111+nelio1977@users.noreply.github.com>
2022-06-28 14:45:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let count= 0;
+let countEl= document.getElementById('count-el')
+let currentCount= document.getElementById('current-count')
+let totalCount= document.getElementById('total')
+let newCount= [];
+
+function increment() {
+   count += 1;
+   countEl.textContent= count;
+}
+
+function save() {
+   if(count) {
+      currentCount.textContent += count + " - ";
+      newCount.push(count)
+      countEl.textContent= 0; 
+      count= 0
+   } 
+}
+
+function undo() {
+  if(count){
+     count -= 1;
+  }
+  countEl.textContent= count;
+}
+
+function total() {
+   let total= newCount.reduce((totalCount, count) => {
+      return totalCount + count;
+   }, 0)
+   totalCount.textContent = total;
+   currentCount.textContent= "";
+}
+
+ 
